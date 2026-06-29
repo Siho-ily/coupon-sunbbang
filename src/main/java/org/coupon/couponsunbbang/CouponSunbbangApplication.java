656a51520ce1db9be1b660824fb81d1deb7530b5
KimYoungQ <dudeb0206@daum.net>
2026-06-29 20:31:00 +0900

    git init

---
+++ src/main/java/org/coupon/couponsunbbang/CouponSunbbangApplication.java
@@ -0,0 +1,13 @@
+package org.coupon.couponsunbbang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CouponSunbbangApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CouponSunbbangApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/coupon/couponsunbbang/CouponSunbbangApplicationTests.java
@@ -0,0 +1,13 @@
+package org.coupon.couponsunbbang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CouponSunbbangApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
